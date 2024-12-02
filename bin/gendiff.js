fbdb388ddb5f2ddc1885b9b511ebd93c7bc4b01b
kava13 <kava3331@mail.ru>
2024-12-03 00:49:27 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,4 +3,11 @@ const { program } = require('commander');
 
 program.name('gendiff').description('  Compares two configuration files and shows a difference.');
 
+program
+  .argument('[filepath1]', 'First file to compare')
+  .argument('[filepath2]', 'Second file to compare')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format');
+//   .option('-h, --help', 'output usage information');
+
 program.parse();
